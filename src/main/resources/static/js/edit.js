557555f78f78103901a0d730f3cdc34ef0e34981
ysj <pirates119@gmail.com>
2021-04-22 14:14:18 +0900

    modify edit style

--- src/main/resources/static/js/edit.js
+++ src/main/resources/static/js/edit.js
@@ -8,7 +8,20 @@ const editOption = {
 	minHeight: 100,
 	maxHeight: 500,
 	focus: true,									// set focus to editable area after initializing summe
-	fontNames: ['돋움', '궁서', '맑은 고딕', '함초롬바탕', 'Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Merriweather']
+	fontNames: ['돋움', '궁서', '맑은 고딕', '함초롬바탕', 'Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Merriweather'],
+	toolbar: [
+		// [groupName, [list of button]]
+		['Font Style', ['fontname']],
+		['style', ['bold', 'italic', 'underline', 'clear']],
+		['font', ['strikethrough']],
+		['fontsize', ['fontsize']],
+		['color', ['color']],
+		['para', ['paragraph']],
+		['height', ['height']],
+		['Insert', ['picture']],
+		['Insert', ['link']],
+		['Misc', ['fullscreen']]
+	]
 	//fontNamesIgnoreCheck: ['Merriweather']
 };
 
