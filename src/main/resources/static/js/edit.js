c839c3a70923c8a0f93a0db5ff37b42c9cefe1ce
ysj <pirates119@gmail.com>
2021-04-22 15:02:42 +0900

    modify files

--- src/main/resources/static/js/edit.js
+++ src/main/resources/static/js/edit.js
@@ -8,9 +8,11 @@ const editOption = {
 	minHeight: 100,
 	maxHeight: 500,
 	focus: true,									// set focus to editable area after initializing summe
+	codemirror: {
+		theme: 'monokai'
+	},
 	fontNames: ['돋움', '궁서', '맑은 고딕', '함초롬바탕', 'Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Merriweather'],
 	toolbar: [
-		// [groupName, [list of button]]
 		['Font Style', ['fontname']],
 		['style', ['bold', 'italic', 'underline', 'clear']],
 		['font', ['strikethrough']],
